2d41c8623e4b8570d0e07dd54f9707509f4d39b2
Ivan <zarwow8@gmail.com>
2019-08-09 11:50:23 +0300

    created componets Main and SearcPic

---
+++ src/components/Main.vue
@@ -0,0 +1,6 @@
+<template></template>
+
+<script></script>
+i
+
+<style scoped></style>;
---
+++ src/components/SearchPic.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <input type="text" placeholder="Ввод" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style scoped>
+input {
+  border: 0px;
+  padding: 5px;
+}
+</style>
+
